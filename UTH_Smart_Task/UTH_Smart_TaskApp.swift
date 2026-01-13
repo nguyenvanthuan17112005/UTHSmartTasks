e8941bf3a4213284d93be3d61567b313a3ed889e
nguyenvanthuan17112005 <thuannv2563@ut.edu.vn>
2026-01-13 10:41:55 +0700

    Hoàn thành dự án UTH_Smart_Task

---
+++ UTH_Smart_Task/UTH_Smart_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  UTH_Smart_TaskApp.swift
+//  UTH_Smart_Task
+//
+//  Created by Nguyễn Văn Thuận on 13/1/26.
+//
+
+import SwiftUI
+
+@main
+struct UTH_Smart_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
